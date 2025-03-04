3e136b049ebb1dc1cf19b1a86c45f4aaae9e6988
Mariam Parvez <mariamparvez.m@gmail.com>
2025-03-05 01:05:55 +0530

    a

--- sections/footer/footer.js
+++ sections/footer/footer.js
@@ -1,44 +1,9 @@
-document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("contact-us");e&&(e.innerHTML=`
-    <div class="connect-background"><span>Let's Connect.</span></div>
-    <div class="contact-us-container">
-        <div class="innerwrap">
-            <section class="contact-header" id="contact-us">
-                <div class="textcenter">
-                    <h5 class="shtext">Contact Us</h5>
-                    <p>We’re here to help your business grow. Reach out to us today for a free consultation and take the first step toward digital success.</p>
-                    <span class="seperator"></span>
-                </div>
-            </section>
-            <section class="contact-container d-flex justify-content-center align-items-start">
-                <div class="embedded-video-container">
-                    <iframe title="The Location of Catalyst Media FZE" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3606.4513862554345!2d55.48858149999999!3d25.322630000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5f5df14e533d%3A0x73cc61cd6d65a7e3!2sSharjah%20Research%20Technology%20and%20Innovation%20Park!5e0!3m2!1sen!2sin!4v1739627030427!5m2!1sen!2sin" 
-                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-                <div class="last m-3">
-                    <div class="sec2innercont">
-                        <div class="contact-details">
-                            <p><strong>Address:</strong> Office B20-174, Sharjah Research Technology & Innovations Park (SRTIP) complex, University City, Sharjah, U.A.E</p>
-                            <p><strong>Phone:</strong> +971526763400</p>
-                            <p><strong>Email:</strong> info@catalystmediafze.com</p>
-                        </div>
-                    </div>
-                    <div class="contact-form">
-                        <h6 class="contact-form-title">Let’s create something amazing together.<br><strong>Contact us today!</strong></h6>
-                        <form action="https://formsubmit.co/info@catalystmediafze.com" method="POST">
-                            <div class="d-flex gap-2">
-                                <input class="form-control" type="text" name="first_name" placeholder="First Name" required>
-                                <input class="form-control" type="text" name="last_name" placeholder="Last Name">
-                            </div>
-                            <div class="d-flex gap-2 mt-2">
-                                <input class="form-control" type="email" name="email" placeholder="Email" required>
-                                <input class="form-control" type="tel" name="phone" placeholder="Contact Number" required>
-                            </div>
-                            <textarea class="form-control mt-2" name="message" cols="30" rows="7" placeholder="Your message here..."></textarea>
-                            <div><input class="btn btn-primary mt-2" type="submit" value="Send"></div>
-                        </form>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("footer");e&&(e.innerHTML=`
+        <div class="d-flex align-items-center flex-column mt-5 container-fluid p-5 text-center" id=footer><img alt="Website's Logo" src=../assets/logo.svg>
+        <h1 class="mt-2 mb-2">Catalyst Media FZE</h1>
+        <div class="d-flex mb-4 justify-content-between mt-1 footer-social-icons">
+            <a href=https://x.com/CatalystMedia24><img alt="Follow us on X" src=../assets/socials/x.svg class=footer-social-icon> </a><a href=https://www.instagram.com/catalystmediafze/><img alt="Follow us on Instagram" src=../assets/socials/instagram.svg class=footer-social-icon>            </a>
+            <a href="https://www.linkedin.com/company/catalyst-media-fze/posts/?feedView=all"><img alt="Follow us on LinkedIn" src=../assets/socials/linkedin-in.svg class=footer-social-icon></a>
+        </div><span class=seperator style="border-bottom:1px solid #fff"></span>
+        <p>Copyright ©2025 All rights reserved by Catalyst Media FZE</p></div>
 `);let t=document.querySelector(".connect-background");t&&document.addEventListener("scroll",()=>{let e=window.scrollY;t.style.backgroundPosition=0!==e?`calc(50% + ${e}px) calc(50% + ${e}px)`:""})});
